9009881c3de673d24ebc89d191dc18d32ed5ccee
yangjiawang <865902964@qq.com>
2018-04-26 11:14:27 +0800

    类型转换

---
+++ src/java1/VerDemo.java
@@ -0,0 +1,23 @@
+package java1;
+
+public class VerDemo {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+byte b=127;
+int age=188888888;
+long l=1000;
+float f=99f;
+double d=100.99;
+d=100d;
+
+boolean isMarried =true;
+isMarried=false;
+
+char c='a';
+c='中';
+String S="杨佳旺";
+
+	}
+
+}
---
+++ src/java1/turn.java
@@ -0,0 +1,21 @@
+package java1;
+
+public class turn {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+     int   agt=9999;//直接复制
+     long  l=agt;
+     System.out.println(l);
+    agt=(int)l;//强制
+    char c='b';
+    int i;
+    i=c;
+    System.out.println(i);
+System.out.println(agt);    
+int a=200;
+     c=(char)a;
+     System.out.println(c);
+	}
+
+}
